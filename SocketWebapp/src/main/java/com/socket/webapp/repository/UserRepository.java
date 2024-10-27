74baaca9c4879004ac28987d48b3d542b694aeb3
KorolER <elizavetakorol2005@gmail.com>
2024-10-27 14:11:14 +0300

    Commit delete of files

--- SocketWebapp/src/main/java/com/socket/webapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.socket.webapp.repository;
-
-import java.util.Optional;
-
-import com.socket.webapp.model.User;
-
-public interface UserRepository {
-	Optional<User> findByUsername(String username);
-}
--- SocketWebapp/src/main/resources/query.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-(
-	id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	username varchar(15) NOT NULL UNIQUE,
-	password text NOT NULL
-);
-DROP TABLE users;
-insert into users(username, "password") values ('user1', 'password');
